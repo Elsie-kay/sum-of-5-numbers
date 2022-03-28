8b3dbac3b7d786f3dbc4fff015bdae024511c6c6
Elsie-kay <101639829+Elsie-kay@users.noreply.github.com>
2022-03-28 17:57:56 +0300

    Add files via upload

---
+++ sum of5 numers.c
@@ -0,0 +1,16 @@
+//program to find sum of five numbers
+#include<stdio.h>
+
+int main()
+{
+int a= 45;
+int b= 12;
+int z=  7;
+int e= 21;
+int m= 51;
+int sum;
+sum=a+b+z+e+m;
+printf("sum of five numbers is %d",sum);
+
+return '0';
+}
